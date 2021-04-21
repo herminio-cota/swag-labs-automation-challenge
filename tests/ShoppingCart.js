84b64965e0aef319402e60ca5fdb4c9ccb13025d
Herminio Cota Perea <Herminio.Perea@ibm.com>
2021-04-21 15:24:24 -0500

    update shopping cart page and test for refactoring

--- tests/ShoppingCart.js
+++ tests/ShoppingCart.js
@@ -1,6 +1,5 @@
 import LoginPage from '../pages/LoginPage';
 import ProductPage from '../pages/ProductPage';
-import ShoppingCartElement from '../elements/ShoppingCartElement';
 import ShoppingCartPage from '../pages/ShoppingCartPage';
 
 const userData = require('../data/usersData.json');
@@ -10,43 +9,27 @@ const testVariablesData = require('../data/testsVariablesData.json');
 fixture`ADD ITEMS TO SHOPPING CART TEST`
     .page`https://www.saucedemo.com`
     .beforeEach(async t => {
-        await t
-        .typeText(LoginPage.usernameInput, userData.standar)
-        .typeText(LoginPage.passwordInput, userData.general_password)
-        .click(LoginPage.loginButton);
+        await LoginPage.login(userData.standar, userData.general_password);
     });
 
 //Expected: Validate all the items that have been added to the shopping cart.
 test('Add multiple items to the shopping cart', async t => {
 
-    const totalItems = await ProductPage.inventory.child().count;
     const itemsAdded = [];
 
     //Add the half of products displayed in Products page
-    for (let i = 0; i < totalItems / 2; i++) {
-
-        let itemName = await ProductPage.inventory.child(i).find('div').withAttribute('class', 'inventory_item_name').textContent;
-        let itemPrice = await ProductPage.inventory.child(i).find('div').withAttribute('class', 'inventory_item_price').textContent;
-        let itemAddButton = await ProductPage.inventory.child(i).find('button');
-
-        itemsAdded.push({ "itemName": itemName, "itemPrice": itemPrice });
-
-        await t.click(itemAddButton).expect(itemAddButton.textContent).eql(expectedData.shoppingCarTest.textRemoveButton);
-    }
-
-    await t
-        .click(ShoppingCartElement.shoppingCartButton);
+    await ProductPage.selectMultipleItems(itemsAdded);
+    await ProductPage.clickOnShopingCartButton();
 
     //Verify each name and price of the products added in Shooping Cart Page
-    let totalShopingCartItems = ShoppingCartPage.cartList.count;
+    let totalShopingCartItems = await ShoppingCartPage.getTotalItemsInShoppingCart();
 
     for (let i = 0; i < totalShopingCartItems - 1; i++) {
 
-        let itemName = await ShoppingCartPage.cartList.nth(i).find('div').withAttribute('class', 'inventory_item_name').textContent;
-        let itemPrice = await ShoppingCartPage.cartList.nth(i).find('div').withAttribute('class', 'inventory_item_price').textContent;
+        let item = ShoppingCartPage.getItemInShopingCartByIndex(i);
 
-        await t.expect(itemName).eql(itemsAdded[i].itemName)
-            .expect(itemPrice).eql(itemsAdded[i].itemPrice);
+        await t.expect(item.itemName).eql(itemsAdded[i].itemName)
+            .expect(item.itemPrice).eql(itemsAdded[i].itemPrice);
     }
 
 });
@@ -55,26 +38,17 @@ test('Add multiple items to the shopping cart', async t => {
 test('Add the specific product ‘Sauce Labs Onesie’ to the shopping cart', async t => {
 
     //Add only the product with the name ‘Sauce Labs Onesie’ in products page
-    ProductPage.setSpecificProductByName(testVariablesData.shoppingCarTest.specificProduct);
-    let itemProduct = await ProductPage.getSpecificProductByName();
-    let itemName = await itemProduct.find('div').withAttribute('class', 'inventory_item_name').textContent;
-    let itemPrice = await itemProduct.find('div').withAttribute('class', 'inventory_item_price').textContent;
-    let itemAddButton = await itemProduct.find('button');
+    let itemAdded = await ProductPage.addSpecificProductToShoppingCartByName(testVariablesData.shoppingCarTest.specificProduct);
+    let itemAddButton = await ProductPage.getAddButton();
+    await t.expect(itemAddButton.textContent).eql(expectedData.shoppingCarTest.textRemoveButton);
 
-    let itemAdded = { "itemName": itemName, "itemPrice": itemPrice };
-
-    await t.click(itemAddButton).expect(itemAddButton.textContent).eql(expectedData.shoppingCarTest.textRemoveButton);
-
-    await t.click(ShoppingCartElement.shoppingCartButton);
+    await ProductPage.clickOnShopingCartButton();
 
     // Validate tehe name and the price of the product added in the shoppin cart
-    ShoppingCartPage.setSpecificProductByName(testVariablesData.shoppingCarTest.specificProduct);
-    let itemShoppingCartProduct = await ShoppingCartPage.getSpecificProductByName();
-    let itemNameShopingCart = await itemShoppingCartProduct.find('div').withAttribute('class', 'inventory_item_name').textContent;
-    let itemPriceShopingCart = await itemShoppingCartProduct.find('div').withAttribute('class', 'inventory_item_price').textContent;
+    let itemShoppingCart = await ShoppingCartPage.getItemFromSpecificProductByName(testVariablesData.shoppingCarTest.specificProduct);
 
-    await t.expect(itemNameShopingCart).eql(itemAdded.itemName)
-        .expect(itemPriceShopingCart).eql(itemAdded.itemPrice);
+    await t.expect(itemShoppingCart.itemName).eql(itemAdded.itemName)
+        .expect(itemShoppingCart.itemPrice).eql(itemAdded.itemPrice);
 
 });
 
